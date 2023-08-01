e3e7e24831269ee2aed10aa6c137a0540bf0d625
tudoi123 <140226945+tudoi123@users.noreply.github.com>
2023-08-01 12:46:20 +0700

    e

--- include/topbar.php
+++ include/topbar.php
@@ -1,3 +1,4 @@
+
 <?php
 	// session_destroy();
 	// unset('dangnhap');
@@ -36,170 +37,175 @@
 		
  		header('Location:index.php?quanly=giohang');
 	}
-?>
+?> 
 
 <!-- top-header -->
-<div class="agile-main-top">
-    <div class="container-fluid">
-        <div class="row main-top-w3l py-2">
-            <div class="col-lg-4 header-most-top">
+	<div class="agile-main-top">
+		<div class="container-fluid">
+			<div class="row main-top-w3l py-2">
+				<div class="col-lg-4 header-most-top">
+					
+				</div>
+				<div class="col-lg-8 header-right mt-lg-0 mt-2">
+					<!-- header lists -->
+					<ul>
 
-            </div>
-            <div class="col-lg-8 header-right mt-lg-0 mt-2">
-                <!-- header lists -->
-                <ul>
-
-                    <?php
+						<?php
 						if(isset($_SESSION['dangnhap_home'])){ 
 						
 						?>
-                    <li class="text-center border-right text-white">
-                        <a href="index.php?quanly=xemdonhang&khachhang=<?php echo $_SESSION['khachhang_id'] ?>"
-                            class="text-white">
-                            <i class="fas fa-truck mr-2"></i>Xem đơn hàng : <?php echo $_SESSION['dangnhap_home'] ?></a>
-                    </li>
-                    <?php
+						<li class="text-center border-right text-white">
+							<a href="index.php?quanly=xemdonhang&khachhang=<?php echo $_SESSION['khachhang_id'] ?>" class="text-white">
+								<i class="fas fa-truck mr-2"></i>Xem đơn hàng : <?php echo $_SESSION['dangnhap_home'] ?></a>
+						</li>
+						<?php
 					}
 						?>
+						
+						<li class="text-center border-right text-white">
+							<a href="#" data-toggle="modal" data-target="#dangnhap" class="text-white">
+								<i class="fas fa-sign-in-alt mr-2"></i> Đăng nhập </a>
+						</li>
+						<li class="text-center text-white">
+							<a href="#" data-toggle="modal" data-target="#dangky" class="text-white">
+								<i class="fas fa-sign-out-alt mr-2"></i>Đăng ký </a>
+						</li>
+					</ul>
+					<!-- //header lists -->
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- modals -->
+	<!-- log in -->
+	<div class="modal fade" id="dangnhap" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title text-center">Đăng nhập</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<form action="#" method="post">
+						<div class="form-group">
+							<label class="col-form-label">Email</label>
+							<input type="text" class="form-control" placeholder=" " name="email_login" required="">
+						</div>
+						<div class="form-group">
+							<label class="col-form-label">Mật khẩu</label>
+							<input type="password" class="form-control" placeholder=" " name="password_login" required="">
+						</div>
+						<div class="right-w3l">
+							<input type="submit" class="form-control" name="dangnhap_home" value="Đăng nhập">
+						</div>
+						
+						<p class="text-center dont-do mt-3">Chưa có tài khoản?
+							<a href="#" data-toggle="modal" data-target="#dangky">
+								Đăng ký</a>
+						</p>
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
 
-                    <li class="text-center border-right text-white">
-                        <a href="#" data-toggle="modal" data-target="#dangnhap" class="text-white">
-                            <i class="fas fa-sign-in-alt mr-2"></i> Đăng nhập </a>
-                    </li>
-                    <li class="text-center text-white">
-                        <a href="#" data-toggle="modal" data-target="#dangky" class="text-white">
-                            <i class="fas fa-sign-out-alt mr-2"></i>Đăng ký </a>
-                    </li>
-                </ul>
-                <!-- //header lists -->
-            </div>
-        </div>
-    </div>
-</div>
-<!-- modals -->
-<!-- log in -->
-<div class="modal fade" id="dangnhap" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title text-center">Đăng nhập</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="#" method="post">
-                    <div class="form-group">
-                        <label class="col-form-label">Email</label>
-                        <input type="text" class="form-control" placeholder=" " name="email_login" required="">
-                    </div>
-                    <div class="form-group">
-                        <label class="col-form-label">Mật khẩu</label>
-                        <input type="password" class="form-control" placeholder=" " name="password_login" required="">
-                    </div>
-                    <div class="right-w3l">
-                        <input type="submit" class="form-control" name="dangnhap_home" value="Đăng nhập">
-                    </div>
-
-                    <p class="text-center dont-do mt-3">Chưa có tài khoản?
-                        <a href="#" data-toggle="modal" data-target="#dangky">
-                            Đăng ký</a>
-                    </p>
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-
-<!-- register -->
-<div class="modal fade" id="dangky" tabindex="-1" role="dialog" aria-hidden="true">
-    <div class="modal-dialog" role="document">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title">Đăng ký</h5>
-                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                    <span aria-hidden="true">&times;</span>
-                </button>
-            </div>
-            <div class="modal-body">
-                <form action="" method="post">
-                    <div class="form-group">
-                        <label class="col-form-label">Tên khách hàng</label>
-                        <input type="text" class="form-control" placeholder=" " name="name" required="">
-                    </div>
-                    <div class="form-group">
-                        <label class="col-form-label">Email</label>
-                        <input type="email" class="form-control" placeholder=" " name="email" required="">
-                    </div>
-                    <div class="form-group">
-                        <label class="col-form-label">Phone</label>
-                        <input type="text" class="form-control" placeholder=" " name="phone" required="">
-                    </div>
-                    <div class="form-group">
-                        <label class="col-form-label">Address</label>
-                        <input type="text" class="form-control" placeholder=" " name="address" required="">
-                    </div>
-                    <div class="form-group">
-                        <label class="col-form-label">Password</label>
-                        <input type="password" class="form-control" placeholder=" " name="password" required="">
-                        <input type="hidden" class="form-control" placeholder="" name="giaohang" value="0">
-                    </div>
-                    <div class="form-group">
-                        <label class="col-form-label">Ghi chú</label>
-                        <textarea class="form-control" name="note"></textarea>
-                    </div>
-
-                    <div class="right-w3l">
-                        <input type="submit" class="form-control" name="dangky" value="Đăng ký">
-                    </div>
-
-                </form>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- //modal -->
-<!-- //top-header -->
-
-
-<!-- header-bottom-->
-<div class="header-bot">
-    <div class="container">
-        <div class="row header-bot_inner_wthreeinfo_header_mid">
-            <!-- logo -->
-
-            <!-- //logo -->
-            <!-- header-bot -->
-            <div class="col-md-9 header mt-4 mb-md-0 mb-4">
-
-                <div class="row">
-                    <!-- search -->
-                    <div class="col-10 agileits_search">
-                        <form class="form-inline" action="index.php?quanly=timkiem" method="POST">
-                            <input class="form-control mr-sm-2" name="search_product" type="search"
-                                placeholder="Tìm kiếm sản phẩm" aria-label="Search" required>
-                            <button class="btn my-2 my-sm-0" name="search_button" type="submit">Tìm kiếm</button>
-                        </form>
-                    </div>
-                    <!-- //search -->
-                    <!-- cart details -->
-                    <div class="col-2 top_nav_right text-center mt-sm-0 mt-2">
-                        <div class="wthreecartaits wthreecartaits2 cart cart box_1">
-                            <form action="#" method="post" class="last">
-                                <input type="hidden" name="cmd" value="_cart">
-                                <input type="hidden" name="display" value="1">
-                                <button class="btn w3view-cart" type="submit" name="submit" value="">
-                                    <i class="fas fa-cart-arrow-down"></i>
-                                </button>
-                            </form>
-                        </div>
-                    </div>
-                    <!-- //cart details -->
-                </div>
-            </div>
-        </div>
-    </div>
-</div>
-<!-- shop locator (popup) -->
-<!-- //header-bottom -->
-<!-- navigation -->
+	<!-- register -->
+	<div class="modal fade" id="dangky" tabindex="-1" role="dialog" aria-hidden="true">
+		<div class="modal-dialog" role="document">
+			<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title">Đăng ký</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+						<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<form action="" method="post">
+						<div class="form-group">
+							<label class="col-form-label">Tên khách hàng</label>
+							<input type="text" class="form-control" placeholder=" " name="name" required="">
+						</div>
+						<div class="form-group">
+							<label class="col-form-label">Email</label>
+							<input type="email" class="form-control" placeholder=" " name="email" required="">
+						</div>
+						<div class="form-group">
+							<label class="col-form-label">Phone</label>
+							<input type="text" class="form-control" placeholder=" " name="phone"  required="">
+						</div>
+						<div class="form-group">
+							<label class="col-form-label">Address</label>
+							<input type="text" class="form-control" placeholder=" " name="address"  required="">
+						</div>
+						<div class="form-group">
+							<label class="col-form-label">Password</label>
+							<input type="password" class="form-control" placeholder=" " name="password"  required="">
+							<input type="hidden" class="form-control" placeholder="" name="giaohang"  value="0">
+						</div>
+						<div class="form-group">
+							<label class="col-form-label">Ghi chú</label>
+							<textarea class="form-control" name="note"></textarea>
+						</div>
+						
+						<div class="right-w3l">
+							<input type="submit" class="form-control" name="dangky" value="Đăng ký">
+						</div>
+					
+					</form>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- //modal -->
+	<!-- //top-header -->
+	
+	
+	<!-- header-bottom-->
+	<div class="header-bot">
+		<div class="container">
+			<div class="row header-bot_inner_wthreeinfo_header_mid">
+				<!-- logo -->
+				<div class="col-md-3 logo_agile">
+				<a class="nav-item mx-2" href="index.php">
+                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="30" fill="currentColor" class="bi bi-apple" viewBox="0 0 16 16">
+                            <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z"/>
+                            <path d="M11.182.008C11.148-.03 9.923.023 8.857 1.18c-1.066 1.156-.902 2.482-.878 2.516.024.034 1.52.087 2.475-1.258.955-1.345.762-2.391.728-2.43Zm3.314 11.733c-.048-.096-2.325-1.234-2.113-3.422.212-2.189 1.675-2.789 1.698-2.854.023-.065-.597-.79-1.254-1.157a3.692 3.692 0 0 0-1.563-.434c-.108-.003-.483-.095-1.254.116-.508.139-1.653.589-1.968.607-.316.018-1.256-.522-2.267-.665-.647-.125-1.333.131-1.824.328-.49.196-1.422.754-2.074 2.237-.652 1.482-.311 3.83-.067 4.56.244.729.625 1.924 1.273 2.796.576.984 1.34 1.667 1.659 1.899.319.232 1.219.386 1.843.067.502-.308 1.408-.485 1.766-.472.357.013 1.061.154 1.782.539.571.197 1.111.115 1.652-.105.541-.221 1.324-1.059 2.238-2.758.347-.79.505-1.217.473-1.282Z"/>
+                        </svg>
+                    </a>
+				</div>
+				<!-- //logo -->
+				<!-- header-bot -->
+				<div class="col-md-9 header mt-4 mb-md-0 mb-4">
+					
+					<div class="row">
+						<!-- search -->
+						<div class="col-10 agileits_search">
+							<form class="form-inline" action="index.php?quanly=timkiem" method="POST">
+								<input class="form-control mr-sm-2" name="search_product" type="search" placeholder="Tìm kiếm sản phẩm" aria-label="Search" required>
+								<button class="btn my-2 my-sm-0" name="search_button" type="submit">Tìm kiếm</button>
+							</form>
+						</div>
+						<!-- //search -->
+						<!-- cart details -->
+						<div class="col-2 top_nav_right text-center mt-sm-0 mt-2">
+							<div class="wthreecartaits wthreecartaits2 cart cart box_1">
+								<form action="#" method="post" class="last">
+									<input type="hidden" name="cmd" value="_cart">
+									<input type="hidden" name="display" value="1">
+									<button class="btn w3view-cart" type="submit" name="submit" value="">
+										<i class="fas fa-cart-arrow-down"></i>
+									</button>
+								</form>
+							</div>
+						</div>
+						<!-- //cart details -->
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- shop locator (popup) -->
+	<!-- //header-bottom -->
+	<!-- navigation -->
