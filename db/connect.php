544698f59630bffec3584a2043da404e4d49518e
Duy <124870452+STEPY1@users.noreply.github.com>
2023-08-10 14:40:05 +0700

    .

--- db/connect.php
+++ db/connect.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("us-cdbr-east-06.cleardb.net","b1c39203325275","2d57df93","heroku_b1e23109d0592c5");
+$con = mysqli_connect("localhost","root","","bandienmay");
 
 // Check connection
 if (mysqli_connect_errno())
