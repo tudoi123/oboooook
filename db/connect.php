7138742c3efda104593b31c41729b7de1c0a42a6
tudoi123 <140226945+tudoi123@users.noreply.github.com>
2023-08-11 12:43:04 +0700

    s

--- db/connect.php
+++ db/connect.php
@@ -1,5 +1,5 @@
 <?php
-$con = mysqli_connect("localhost","root","","shop book");
+$con = mysqli_connect("us-cdbr-east-06.cleardb.net","bb8fd0e69abcc4","abc1ef46","heroku_c9c3c865a304ebb");
 
 // Check connection
 if (mysqli_connect_errno())
